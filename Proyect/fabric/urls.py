c1857668014a8a34deabe4d13955e513a825d443
Joseph <joseph.zamora64@yahoo.com>
2019-04-21 01:44:52 -0600

    * Added button to save page to pdf

--- Proyect/fabric/urls.py
+++ Proyect/fabric/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import FabricView
+from .views import FabricView, render_pdf_view
 
 urlpatterns = [
     path('', FabricView.as_view()),
+    path('save_pdf/', render_pdf_view, name="save"),
 ]
